b52a148f73dacf25acd15f86b33c255600b148e3
Ramon Bartl <rb@ridingbytes.com>
2017-11-13 17:12:18 +0100

    Basic SENAITE package bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# senaite.sync
-Synchronization Solutions to migrate data between two SENAITE instances
+<div align="center">
+
+  <a href="https://github.com/senaite/senaite.ci">
+    <img src="Senaite_LIMS_1000mm.png" alt="SENAITE" height="64" />
+  </a>
+  <h1>SENAITE SYNC</h1>
+
+  <p>Synchronization Solution to migrate data between two SENAITE instances</p>
+
+</div>
---
+++ setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+version = '1.0.1'
+
+
+setup(
+    name='senaite.sync',
+    version=version,
+    description="SENAITE SYNC",
+    long_description="Changelog\n" +
+                     "=========\n" +
+                     open("docs/Changelog.rst").read() + "\n" +
+                     "\n\n" +
+                     "Authors and maintainers\n" +
+                     "-----------------------\n\n" +
+                     "- Ramon Bartl (RIDING BYTES) <rb@ridingbytes.com>",
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+    ],
+    keywords='',
+    author='SENAITE Foundation',
+    author_email='hello@senaite.com',
+    url='https://github.com/senaite/senaite.sync',
+    license='GPLv3',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    namespace_packages=['senaite'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'senaite.api',
+        'senaite.jsonapi',
+    ],
+    extras_require={},
+    entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
