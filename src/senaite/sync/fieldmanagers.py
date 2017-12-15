6c7585ba97458f55fd57564063cb1216369ef0be
Juan Gallostra <jgallostra@naralabs.com>
2017-12-15 13:03:11 +0100

    StringField manager for handling the set of values in String Fields

---
+++ src/senaite/sync/fieldmanagers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017-2017 SENAITE LIMS.
+
+from zope import interface
+
+from senaite.jsonapi.fieldmanagers import ATFieldManager
+from senaite.jsonapi.interfaces import IFieldManager
+
+
+class StringFieldManager(ATFieldManager):
+    """Adapter to set/get the value of String Fields
+    """
+    interface.implements(IFieldManager)
+
+    def set(self, instance, value, **kw):
+        """Set the value of the field
+        """
+        if value is None:
+            value = ''
+        return self._set(instance, value, **kw)
