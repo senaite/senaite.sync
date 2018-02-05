e346f3b61381cc43d70cef883c716587fcdb567e
Juan Gallostra <jgallostra@naralabs.com>
2018-02-05 13:10:02 +0100

    Implement upgrade step machinery

---
+++ src/senaite/sync/config.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of SENAITE.LIMS
+#
+# Copyright 2018 by it's authors.
+# Some rights reserved. See LICENSE and CONTRIBUTING.
+
+PROJECTNAME = "senaite.sync"
