62f2f16b58b5c88b0aba0d0752e116d1726e2877
Nihadness <1992.nihad@gmail.com>
2018-02-27 12:51:27 +0100

    Get State Variable Names from Workflow Definitions.

--- src/senaite/sync/utils.py
+++ src/senaite/sync/utils.py
@@ -82,10 +82,11 @@ def review_history_imported(obj, review_history, wf_tool=None):
     if wf_tool is None:
         wf_tool = api.get_tool('portal_workflow')
 
-    state = review_history.get('review_state')
+    state_variable = wf_tool.variables.getStateVar()
+    state = review_history.get(state_variable)
     current_rh = wf_tool.getInfoFor(obj, 'review_history', '')
     for rh in current_rh:
-        if rh.get('review_state') == state:
+        if rh.get(state_variable) == state:
             return True
 
     return False
